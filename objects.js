fa95ca2ca4373689dbd86395461b71009b36e0be
Paul Riverain <paulriverain@yahoo.com>
2018-12-28 20:42:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist1: '1 song', artist2: '2 song'}
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
   return playlist
 }
